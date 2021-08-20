6aba20b1cb46cc9e27374519ee306e6db0c18de5
Jesslenn <jessica.lennick@gmail>
2021-08-20 14:37:35 -0400

    task: began to integrate Google Vision API into camera component

--- config/firebase.js
+++ config/firebase.js
@@ -8,5 +8,6 @@ firebase.initializeApp({
   projectId: Environment["FIREBASE_PROJECT_ID"],
   storageBucket: Environment["FIREBASE_STORAGE_BUCKET"],
   messagingSenderId: Environment["FIREBASE_MESSAGING_SENDER_ID"],
+  googleCloudVisionAPIKey: Environment["GOOGLE_CLOUD_VISION_API_KEY"]
 });
 export default firebase;
