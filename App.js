253df50185a417382fb0b99104572160f86727f4
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2021-08-24 17:43:05 -0400

    task: began adding functionality to firebase login and sign up; did rudimentary routing work

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Login from "./screens/Login";
 import MainScreen from "./screens/MainScreen";
 import SplashScreen from "./screens/SplashScreen";
 import Signup from "./screens/Signup"
+import auth from '@react-native-firebase/auth';
+import { AuthContext } from "./AuthProvider";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return <MainScreen />;
+  const {user, setUser} = useContext(AuthContext);
+  const [initializing, setInitializing] = useState(true);
+
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if(initializing) setInitializing(false);
+  }
+  
+  //is onAuthStateChanged decremented?
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) return null;
+
+  return (
+  <NavigationContainer>
+    { user ? <MainScreen /> : <Login /> }
+  </NavigationContainer>
+  );
 }
