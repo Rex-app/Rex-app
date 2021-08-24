a353746d4245d2d3388a003512d0c288f0a7dd9e
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-23 23:04:38 -0700

    Refactor: Delete unused imports

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import CameraComponent from "./CameraComponent";
 
+// Screen imports
 import MainScreen from './screens/MainScreen'
 import SplashScreen from './screens/SplashScreen'
 
