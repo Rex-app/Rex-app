04ce13a4f257fe9b273ffa3247f9cd7cd25f211b
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-26 09:57:49 -0700

    Refactor: Add notes to explain code; add two more examples of dummy data

--- dummyDataTesting/dataParsingTests.js
+++ dummyDataTesting/dataParsingTests.js
@@ -1,4 +1,4 @@
-// import fluoxetineDummyData from "./FluoxetineDummyData";
+// DUMMY DATA
 const fluoxetineDummyData = require("./FluoxetineDummyData");
 
 const txtAnnotationsArr = fluoxetineDummyData.responses[0].textAnnotations
@@ -11,4 +11,18 @@ for (const word of txtAnnotationsArr) {
 }
 console.log(filteredData)
 
+/*
+GOALS:
+const textToSpeak = "Take 1 capsule by mouth every day in the morning"
+
+const sortedData = [
+  [{Take},{1}, {capsule} ], => all of these will the same or similar y-values (the x values increase from left to right)
+  [{by}, {mouth}, {every}, {day}], => all of these will have a LARGER y-value than the array before it
+  [{in}, {the}, {morning}] => all of these will have the largest y-value
+]
+
+TO TEST:
+run the following command in the terminal:
+node dummyDataTesting/dataParsingTests.js
+*/
 
