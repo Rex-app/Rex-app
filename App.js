1e917f0a29055575d7783df06cb80e238d95813b
Jessica Lennick <77980935+jesslenn@users.noreply.github.com>
2021-08-31 12:14:08 -0400

    Task: initial code for Splashscreen

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState }  from 'react';
 import AppLoading from 'expo-app-loading';
-
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
 import Routes from './navigation/index';
+import * as Speech from 'expo-speech';
 
 export default function App() {
-  return <Routes />;
-}
-
 //setInterval to however long the audio is
-/*
+
 const [appIsReady, setAppIsReady] = useState(false);
 
 useEffect(() => {
   async function splashScreen () {
     try {
-      setInterval BLAH
+
+      await new Promise(resolve => setInterval(resolve, 8000)); //BLAH
     } catch (e) {
       console.log(e);
     } finally {
@@ -36,9 +36,9 @@ useEffect(() => {
 
   return (
     <View
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-      onLayout={onLayoutRootView}>
-      ***SPLASH SCREEN***
-    </View>
+    onLayout={onLayoutRootView}>
+   <Routes /> 
+   </View>
   );
-} */
+}
+
