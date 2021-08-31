45808da06cee2d273b0aff67235afca33017fb42
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-31 09:35:35 -0700

    Refactor: Delete unused comments

--- App.js
+++ App.js
@@ -6,39 +6,3 @@ import Routes from './navigation/index';
 export default function App() {
   return <Routes />;
 }
-
-//setInterval to however long the audio is
-/*
-const [appIsReady, setAppIsReady] = useState(false);
-
-useEffect(() => {
-  async function splashScreen () {
-    try {
-      setInterval BLAH
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setAppIsReady(true);
-    }
-  }
-  splashScreen();
-}, []);
-
- const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  return (
-    <View
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-      onLayout={onLayoutRootView}>
-      ***SPLASH SCREEN***
-    </View>
-  );
-} */
