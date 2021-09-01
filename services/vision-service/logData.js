51e465ee505ef77fe6e4be440c16bbc5ab850cf3
Emily Higgins <ehhiggins37@gmail.com>
2021-09-01 11:50:35 -0400

    task: adding explanation to logData logic

--- services/vision-service/logData.js
+++ services/vision-service/logData.js
@@ -1,7 +1,9 @@
 import prescriptionParser from "./prescriptionParser"
 
 export default function logData(googleResponse) {
+  //checking if there is a valid googleResponse
   if (googleResponse) {
+    //this logic is checking if the googleResponse is an "empty" return aka no text was found in the picture. An "empty" google response is still a complex nested object, so the logic is checking that nested object is empty of text. If no text is found in the picture, the app asks the user to retake the photo.
     if (typeof (googleResponse.responses[0]) === "object" && Array.isArray(googleResponse.responses) && (Object.keys(googleResponse.responses[0]))[0] === undefined) {
       return "There was an error. Please retake photo."
     } else {
